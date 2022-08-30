6f9cc575f02feaa1695194fd59e484da9641be27
LucileGB <lgarrigoux@hotmail.com>
2022-08-30 18:03:22 +0200

    Implemented database functionalities and test.

---
+++ app/database.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+from functools import wraps
+
+
+class Database():
+    def __init__(self, db_name="database.db"):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS prompts (body, collection)")
+        self.conn.close()
+
+    def db_handler(func):
+        """
+        Note: @wraps is mandatory for docstrings to work on wrapped functions.
+        We must return result, for func(), else the function will be returned
+        instead of the values it returns.
+        """
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            self.conn = sqlite3.connect(self.db_name)
+            self.cursor = self.conn.cursor()
+
+            result = func(self, *args, **kwargs)
+            
+            self.conn.close()
+            return result
+
+        return wrapper
+
+    @db_handler
+    def get_prompts_list(self):
+        response = self.cursor.execute("select * from prompts")
+        return response.fetchall()
+
+    @db_handler
+    def create_prompt(self, prompt):
+        #TODO: implement duplicate spotting
+        self.cursor.execute("""INSERT INTO prompts(body, collection)
+                            VALUES(?, 'null')""", (prompt,))
+        self.conn.commit()
+
+    @db_handler
+    def create_prompt_list(self, prompts):
+        #TODO: implement duplicate spotting
+        prompts = [(pr,) for pr in prompts]
+        self.cursor.executemany("""INSERT INTO prompts(body, collection)
+                            VALUES(?, 'null')""", prompts)
+        self.conn.commit()
+
+    @db_handler
+    def delete_prompt(self, prompt):
+        self.cursor.execute("""DELETE FROM prompts WHERE
+                            body = ?""", (prompt,))
+        self.conn.commit()
+
+    @db_handler
+    def edit_prompt_body(self, prompt, new_prompt):
+        self.cursor.execute("""UPDATE prompts
+                            SET body = ?
+                            WHERE body = ?""", (new_prompt, prompt))
+        self.conn.commit()
+
+    @db_handler
+    def edit_prompt_collection(self, prompt, new_collection):
+        self.cursor.execute("""UPDATE prompts
+                            SET collection = ?
+                            WHERE body = ?""", (new_collection, prompt))
+        self.conn.commit()
